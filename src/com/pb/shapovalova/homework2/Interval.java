6e891cce4f59b9da1470aa1953a06a60dde32cb7
dn141286son1 <olga.shapovalova.08@privatbank.ua>
2021-10-15 00:51:47 +0300

    init commit

---
+++ src/com/pb/shapovalova/homework2/Interval.java
@@ -0,0 +1,13 @@
+package com.pb.shapovalova.homework2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        int x;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        x = scan.nextInt();
+
+    }
+}
