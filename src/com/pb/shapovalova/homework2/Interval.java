b4b86f2df9ec2d14e0180273d7c8bb57f17dddcd
dn141286son1 <olga.shapovalova.08@privatbank.ua>
2021-10-15 21:53:56 +0300

    init commit

--- src/com/pb/shapovalova/homework2/Interval.java
+++ src/com/pb/shapovalova/homework2/Interval.java
@@ -8,6 +8,22 @@ public class Interval {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите целое число");
         x = scan.nextInt();
+        if (x >= 0 && x <= 100) {
+            if (x <= 14) {
+                System.out.println("Введенное число находится в промежутке [0 - 14]");
+            }
+            if (x >= 51) {
+                System.out.println("Введенное число находится в промежутке [51 - 100]");
+            }
+            if (x >= 15 && x <= 35) {
+                System.out.println("Введенное число находится в промежутке [15 - 35]");
+            } if (x>=36 && x<=50) {
+                System.out.println("Введенное число находится в промежутке [36 - 50]");
+            }
 
+        }
+        else {System.out.println("Введенное число не найдено в интервале [0 - 100]");
+
+        }
     }
 }
