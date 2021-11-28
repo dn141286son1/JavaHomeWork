af96f073607d53e90c30505e88289e28be618555
dn141286son1 <olga.shapovalova.08@privatbank.ua>
2021-11-28 20:07:52 +0200

    init commit

---
+++ src/com/pb/shapovalova/homework10/Main.java
@@ -0,0 +1,43 @@
+package com.pb.shapovalova.homework10;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumBox<Float> floatNumBox = new NumBox<>(5);
+        NumBox<Integer> integerNumBox = new NumBox<>(4);
+
+        floatNumBox.set(0, 1.1f);
+        floatNumBox.set(1, 1.2f);
+        floatNumBox.set(2, 1.3f);
+        floatNumBox.set(3, 1.4f);
+
+        integerNumBox.set(0, 10);
+        integerNumBox.set(1, 11);
+        integerNumBox.set(2, 12);
+
+        try{
+            floatNumBox.add(1.5f);
+        }catch (Exception e){
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        try{
+            integerNumBox.add(13);
+        }catch (Exception e){
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("Среднее арифметическое: " + floatNumBox.average());
+        System.out.println("Значение по индексу: " + floatNumBox.get(1));
+        System.out.println("Максимальный элемент: " + floatNumBox.max());
+        System.out.println("Сумма элементов: " + floatNumBox.sum());
+        System.out.println("Текущее кол-во элементов: " + floatNumBox.length());
+
+        System.out.println("Среднее арифметическое: " + integerNumBox.average());
+        System.out.println("Значение по индексу: " + integerNumBox.get(2));
+        System.out.println("Максимальный элемент: " + integerNumBox.max());
+        System.out.println("Сумма элементов: " + integerNumBox.sum());
+        System.out.println("Текущее кол-во элементов: " + integerNumBox.length());
+    }
+}
---
+++ src/com/pb/shapovalova/homework10/NumBox.java
@@ -0,0 +1,69 @@
+package com.pb.shapovalova.homework10;
+
+public class NumBox <T extends Number> {
+
+    private final T[] numbers;
+
+    public NumBox(int size) {
+        numbers = (T[]) new Number[size];
+    }
+
+    public void add(T num) throws Exception {
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] == null){
+                numbers[i] = num;
+            }else throw new Exception("Массив заполнен!");
+        }
+    }
+
+    public T get(int index){
+        return numbers[index];
+    }
+
+    public int length(){
+        int currentLength = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == null){
+                currentLength++;
+            }
+        }
+        return currentLength;
+    }
+
+    public double average(){
+        double sum = 0;
+        for (T i : numbers) {
+            if (i != null) {
+                sum += i.doubleValue();
+            }
+        }
+        double average = sum / numbers.length;
+        return average;
+    }
+
+    public double sum(){
+        double sum = 0;
+        for (T i : numbers) {
+            if (i != null) {
+                sum += i.doubleValue();
+            }
+        }
+        return sum;
+    }
+
+    public double max(){
+        double max = 0.0;
+        for (T i : numbers) {
+            if (i != null){
+                if (i.doubleValue() > max) {
+                    max = i.doubleValue();
+                }
+            }
+        }
+        return max;
+    }
+
+    public void set(int index, T number) {
+        this.numbers[index] = number;
+    }
+}
